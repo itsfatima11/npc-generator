c19a0f16f8484e0c262daea6fe9627969aea29a9
itsfatima11 <fatemeilanlou11@gmail.com>
2026-07-22 18:30:24 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,3 +12,54 @@ coverage: { reporter: ['text', 'html'] },
 },
 });
 ``
+Skip to content
+itsfatima11
+npc-generator
+Repository navigation
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security and quality
+Insights
+Settings
+Files
+Go to file
+t
+T
+dist
+assets
+index.html
+node_modules
+src
+README.md
+eslint.config.js
+gitignore
+index.html
+package-lock.json
+package.json
+tsconfig.app.json
+tsconfig.app.tsbuildinfo
+tsconfig.json
+tsconfig.node.json
+tsconfig.node.tsbuildinfo
+vite.config.ts
+npc-generator
+/dist/
+author
+itsfatima11
+add .gitignore and project files
+537a8ed
+ · 
+1 hour ago
+Name	Last commit message	Last commit date
+..
+assets
+add .gitignore and project files
+1 hour ago
+index.html
+add .gitignore and project files
+1 hour ago
+Copied! 
